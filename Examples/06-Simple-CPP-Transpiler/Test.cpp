677459c9102dc958beceb51c5f97766b0cd42e98
Abe Pralle <abe.pralle@gmail.com>
2021-11-25 21:40:47 -1000

    [Example] Added Examples/06-Simple-CPP-Transpiler

---
+++ Examples/06-Simple-CPP-Transpiler/Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  double inches, cm;
+  cout << "Simple Test" << endl;
+  inches = 12.0;
+  cm = (inches * 2.54);
+  cout << inches << " inches = " << cm << " cm" << endl;
+  return 0;
+}

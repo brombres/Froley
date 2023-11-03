0b1863ac2f00c0fa27c0feab1c28763b3288e020
Brom Bresenham <brom.bresenham@runegate.com>
2023-11-03 12:11:39 -0400

    [Example 06] Fixed and updated

--- Examples/06-Simple-CPP-Transpiler/Test.cpp
+++ Examples/06-Simple-CPP-Transpiler/Test.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-  double inches, cm;
+  double inches, cm, deg, rad;
   cout << "Simple Test" << endl;
   inches = 12.0;
   cm = (inches * 2.54);
   cout << inches << " inches = " << cm << " cm" << endl;
+  deg = 45.0;
+  rad = ((deg / 180.0) * 3.1416);
+  cout << deg << " degrees = " << rad << " radians" << endl;
   return 0;
 }
